797c8f4d42ffeb1043a6eb1443ca8c198b37e471
claudiasolbes <cssolbes@hotmail.com>
2019-12-08 18:14:53 -0500

    age validation works

--- index.js
+++ index.js
@@ -7,15 +7,21 @@ function selectElements() {
   const addButton = document.querySelector(".add")
   addButton.addEventListener("click", addMember)
   const submitButton = document.querySelector("button[type=submit]")
-  submitButton.addEventListener("submit", submitForm)
+  submitButton.addEventListener("click", submitForm)
 }
 
 function ageValue(){
   const ageForm = document.querySelector("input[name=age]")
   const age = ageForm.value
   const ageNumber = Number(age)
-  // console.log(ageNumber)
   return ageNumber
+}
+
+function relationshipValue(){
+  const relationshipMenu = document.querySelector("select[name=rel]")
+  const relationship = relationshipMenu.value
+  console.log(relationship)
+  return relationship
 }
 
 function addMember(event) {
@@ -27,4 +33,5 @@ function addMember(event) {
 
 function submitForm(event) {
   event.preventDefault()
+  console.log("submitting")
 }
